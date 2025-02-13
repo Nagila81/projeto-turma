7a657cd44946bcbdd4ad0d5577010063b12a4ba5
unknown <nagilapereira8896@digitalcollege.internal>
2025-02-13 07:30:23 -0300

    correção do trabalho

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ function criarTeam() {
     carregarTeams(listaDeTeams);
 }
 
+function adicionarParticipante(nomeTeam) {
+    let teamEncontrado = listaDeTeams.find(team => team.nome === nomeTeam);
+    if (teamEncontrado && teamEncontrado.participantes < teamEncontrado.capacidade) {
+      teamEncontrado.participantes++;
+    }
+    carregarTeams(listaDeTeams);
+  }
+
 function carregarTeams(lista) {
     let teamsGrid = document.querySelector("#teams");
     teamsGrid.innerHTML = '';
@@ -51,18 +59,18 @@ function carregarTeams(lista) {
             <div class="bg-cinza2 rounded-lg p-4">
                 <div class="flex items-center justify-between">
                     <h6 class="text-white text-[18px] font-bold">${team.nome}</h6>
-                    <box-icon name='show' type='solid' class="fill-white cursor-pointer duration-200 hover:fill-rosa"></box-icon>
+                    <box-icon name='show' type='solid' class="fill-white cursor-pointer duration-200 hover:fill-rosa" onclick="mostrarParticipantes('${team.nome}')"></box-icon>
                 </div>
                 <div class="w-[100px] h-[100px] rounded-full bg-cinza1 flex flex-col justify-center items-center m-auto my-[26px]">
                     <h1 class="text-white text-[50px] leading-[50px]">${team.participantes}</h1>
                     <h6 class="font-bold text-white">/ ${team.capacidade}</h6>
                 </div>
                 <div class="flex gap-4">
-                    <button class="flex-1 h-[40px] flex items-center border-2 border-rosa rounded-lg text-white text-[12px] uppercase font-bold relative group">
+                    <button class="flex-1 h-[40px] flex items-center border-2 border-rosa rounded-lg text-white text-[12px] uppercase font-bold relative group" onclick="adicionarParticipante('${team.nome}')">
                         <span class="w-0 h-full absolute top-0 left-0 bg-rosa duration-200 group-hover:w-full"></span>
                         <span class="w-full relative z-10 text-center">Adicionar</span>
                     </button>
-                    <button class="w-[40px] h-[40px] flex justify-center items-center border-2 border-rosa rounded-lg text-white text-[12px] uppercase font-bold relative group">
+                    <button class="w-[40px] h-[40px] flex justify-center items-center border-2 border-rosa rounded-lg text-white text-[12px] uppercase font-bold relative group" onclick="excluirTeam('${team.nome}')">
                         <span class="w-0 h-full absolute top-0 left-0 bg-rosa duration-200 group-hover:w-full"></span>
                         <box-icon name='trash' class="fill-white relative z-10"></box-icon>
                     </button>
@@ -71,3 +79,22 @@ function carregarTeams(lista) {
         `;
     })
 }
+
+function excluirTeam(nomeTeam){
+    listaDeTeams = listaDeTeams.filter(team => team.nome !== nomeTeam);
+    carregarTeams(listaDeTeams);
+}
+
+function buscarTeam() {
+    let inputBuscar = document.querySelector("input[type='text']");
+    let valorBuscar = inputBuscar.value.toLowerCase();
+    let teamsEncontrados = listaDeTeams.filter(team => team.nome.toLowerCase().includes(valorBuscar) || team.participantes.toString().includes(valorBuscar));
+    carregarTeams(teamsEncontrados);
+  }
+
+  function mostrarParticipantes(nomeTeam) {
+    let teamEncontrado = listaDeTeams.find(team => team.nome === nomeTeam);
+    if (teamEncontrado) {
+      alert(Participantes do time ${teamEncontrado.nome}: ${teamEncontrado.participantes});
+    }
+  }
